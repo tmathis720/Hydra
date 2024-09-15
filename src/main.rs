accb5ba583a4278ef5472d709730e1f31be16cbd
tmathis720 <34523436+tmathis720@users.noreply.github.com>
2024-09-14 23:52:39 -0700

    Updated main with print message.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,39 @@
 // Import the necessary modules and types
-use hydra::input::gmsh::GmshParser;
-use hydra::domain::mesh::{Mesh, FaceElementRelation};
 
 fn main() {
     // Load the mesh from a Gmsh file
-    let (nodes, elements, faces) = GmshParser::load_mesh("inputs/test.msh2")
-        .expect("Failed to load mesh");
+    print_mascot()
 
     // Further logic for the simulation goes here...
+}
+
+fn print_mascot() {
+    let hydra_mascot = r#"
+              ___
+           __/{o \\
+        __/  _/    \    ___
+     __/  _/     \  \__/o  \
+    /  \_/ \__/o  \  _     )\_
+    \  o \  _    __/o \___/   \    __
+      \__/ o\___/       \__    \__/o \
+           \__           _\___/  o   )
+             /  __/\     /    \_______/
+           _/__/o   \___/
+         /o      \_/
+         \__/
+
+        HYDRA - v0.2.0
+
+  HYDRA is a Rust-based project 
+    designed to provide a flexible, 
+        modular framework for solving 
+            partial differential equations (PDEs) 
+                using finite volume methods (FVM).
+
+    * Software is still in development! *
+  Visit the repository at: 
+        https://github.com/tmathis720/HYDRA
+    "#;
+
+    println!("{}", hydra_mascot);
 }
